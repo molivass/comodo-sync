a6c2efe3157cf967dda09565ef868c8704dad5e1
molivass <molivassotos@gmail.com>
2026-06-24 23:30:14 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,4 +25,4 @@ language = 'es'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'godot'
-html_static_path = ['comodo-sync/_static']
+html_static_path = ['./_static']
